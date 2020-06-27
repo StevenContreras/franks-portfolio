813f3c1421852e39b4df79d1fb2b0397f08aca0b
Steven Contreras <SFContra@gmail.com>
2020-06-27 14:26:03 -0700

    Set up somee minor CSS

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -10,11 +10,13 @@ class App extends React.Component{
 
     render() {
       return (
-        <dev>
+        <div>
           <h1>Hi Elvis!</h1>
           <h2>Welcome to the Franks Portfolio</h2>
-
-        </dev>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <img src="https://scontent-lax3-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/p750x750/99149724_871725079905785_572190201409699607_n.jpg?_nc_ht=scontent-lax3-1.cdninstagram.com&_nc_cat=105&_nc_ohc=YL6yR-VYBiUAX-7Qjnz&oh=c9117d1ea10bdf159dbaa1add1b9db6f&oe=5F1FA187" alt="khara">
+          </img>
+        </div>
       )
     }
 };
