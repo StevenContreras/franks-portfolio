3390d4c8569a8aff8ad8c1438169507cf4650dd2
Steven Contreras <SFContra@gmail.com>
2020-06-26 22:22:47 -0700

    Set up the foundation to render react components

---
+++ client/src/components/app.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+class App extends React.Component{
+    constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+    };
+
+    render() {
+      return (
+        <dev>
+          <h1>Hi Elvis!</h1>
+          <h2>Welcome to the Franks Portfolio</h2>
+
+        </dev>
+      )
+    }
+};
+
+export default App;
